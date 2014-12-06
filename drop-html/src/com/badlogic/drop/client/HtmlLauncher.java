5259faa60f8f174bc0eb49b3317b9b1a5361df69
Milan <milandeepshergill@gmail.com>
2014-12-05 19:08:52 -0500

    Sample Projects

---
+++ drop-html/src/com/badlogic/drop/client/HtmlLauncher.java
@@ -0,0 +1,20 @@
+package com.badlogic.drop.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.badlogic.drop.Drop;
+
+public class HtmlLauncher extends GwtApplication {
+	@Override
+	public GwtApplicationConfiguration getConfig() {
+		GwtApplicationConfiguration cfg = new GwtApplicationConfiguration(800,
+				480);
+		return cfg;
+	}
+
+	@Override
+	public ApplicationListener getApplicationListener() {
+		return new Drop();
+	}
+}
